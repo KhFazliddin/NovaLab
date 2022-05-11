86b5f4b82f5cc8f733b52f7564cd7d9361b02bd4
KhFazliddin <81314108+KhFazliddin@users.noreply.github.com>
2022-05-11 18:27:42 +0500

    changes

--- src/protectedRoute.js/protectedRoute.js
+++ src/protectedRoute.js/protectedRoute.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 export default function ProtectedRoute() {
       const loggedIn=useSelector(state=>state.auth.loggedIn);
 
-  return loggedIn ? <Outlet/> : <Navigate to="/"/>
+  return loggedIn ? <Outlet/> : <Navigate to="/NovaLab"/>
   
 }
 
